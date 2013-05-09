140f9c36bef4ab58f5ad79fad5c4556d162676e5
Ernesto Mancebo <ernesmancebo@gmail.com>
2013-05-09 15:27:02 -0400

    Agrege un texto

---
+++ ernesto.design.patterns/src/ernesto/design/patterns/beggin/Duck.java
@@ -0,0 +1,13 @@
+package ernesto.design.patterns.beggin;
+
+public class Duck {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Probando git");
+		System.out.println("Parece estar bien");
+	}
+
+}
